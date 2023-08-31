b548af0b4380e41d754795b144332d2efc59d912
ishika1403 <ishikamukherjee14@gmail.com>
2023-08-31 21:47:23 +0530

    Route groups created and seperated layout for auth and other routes

---
+++ src/app/(auth)/auth/layout.js
@@ -0,0 +1,17 @@
+import "../../globals.css";
+import { Poppins } from "next/font/google";
+import { ReduxProvider } from "@/redux/provider";
+
+const poppins = Poppins({ weight: ["400", "500", "700"], subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <ReduxProvider>
+          <div className="wrapperDiv">{children}</div>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "../globals.css";
 import { Poppins } from "next/font/google";
 import { ReduxProvider } from "@/redux/provider";
 import Navbar from "@/components/Navbar";
